276c8a321c20b510f77d19926c9b4b6880c9053a
Juliane Dannberg <judannberg@gmail.com>
2022-02-19 16:42:25 -0500

    properly average viscosity

--- source/simulator/assembly.cc
+++ source/simulator/assembly.cc
@@ -299,6 +299,7 @@ namespace aspect
                                                cell,
                                                scratch.finite_element_values.get_quadrature(),
                                                scratch.finite_element_values.get_mapping(),
+                                               scratch.material_model_inputs.requests_property(MaterialModel::MaterialProperties::viscosity),
                                                scratch.material_model_outputs);
 
     for (unsigned int i=0; i<assemblers->stokes_preconditioner.size(); ++i)
@@ -548,6 +549,7 @@ namespace aspect
                                                cell,
                                                scratch.finite_element_values.get_quadrature(),
                                                scratch.finite_element_values.get_mapping(),
+                                               scratch.material_model_inputs.requests_property(MaterialModel::MaterialProperties::viscosity),
                                                scratch.material_model_outputs);
 
     scratch.finite_element_values[introspection.extractors.velocities].get_function_values(current_linearization_point,
@@ -927,6 +929,7 @@ namespace aspect
                                                cell,
                                                scratch.finite_element_values.get_quadrature(),
                                                scratch.finite_element_values.get_mapping(),
+                                               scratch.material_model_inputs.requests_property(MaterialModel::MaterialProperties::viscosity),
                                                scratch.material_model_outputs);
 
     heating_model_manager.evaluate(scratch.material_model_inputs,
